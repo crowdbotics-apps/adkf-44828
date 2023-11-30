e291b05e34431110128569530cb175d1e1f707fb
Crowdbotics <team@crowdbotics.com>
2023-11-30 20:46:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-heart-44828",
+  url: "https://muddy-heart-44828.botics.co",
+  api: "https://muddy-heart-44828.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
